003e2c0e130178ee799e767436c6bd8a92d90d9e
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2024-05-24 10:05:45 +0100

    Add Alert component

--- src/app/components/Alert.js
+++ src/app/components/Alert.js
@@ -1,6 +1,10 @@
 import * as React from "react";
 import Alert from "@mui/material/Alert";
 
-export default function SimpleAlert({ message, severity }) {
-  return <Alert severity={severity}>{message}</Alert>;
+export default function SimpleAlert({ message, severity, setShowAlert }) {
+  return (
+    <Alert severity={severity} onClose={() => setShowAlert(false)}>
+      {message}
+    </Alert>
+  );
 }
